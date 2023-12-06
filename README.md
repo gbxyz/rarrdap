c2b29883df5ca02bd4f7f0ed4a5bcfa10f856818
Gavin Brown <gavin.brown@fastmail.uk>
2023-12-06 13:41:29 +0000

    fixed!

--- README.md
+++ README.md
@@ -1,37 +1,39 @@
 # NAME
 
-`rarrdap.pl` - a script to generate a set of RDAP responses for ICANN-accredited registrars.
+`rarrdap.pl` - a script to generate a set of RDAP responses for ICANN-accredited
+registrars.
 
 # DESCRIPTION
 
-This script scrapes data from the the IANA registrar ID registry and the Internic site, and
-generates RDAP responses for each ICANN-accredited registrar.
+This script scrapes data from the the ICANN website, and generates RDAP
+responses for each ICANN-accredited registrar.
 
-The RDAP responses are written to disk in a directory which can then be exposed through a web
-server.
+The RDAP responses are written to disk in a directory which can then be exposed
+through a web server.
 
 An example of an RDAP service which provides access to this data may be found at
 [https://registrars.rdap.org](https://registrars.rdap.org), for example:
 
 - [https://registrars.rdap.org/entity/1564-iana](https://registrars.rdap.org/entity/1564-iana)
 
-Entity handles have the "-iana" object tag, as per [https://www.rfc-editor.org/rfc/rfc8521.html](https://www.rfc-editor.org/rfc/rfc8521.html).
+Entity handles have the "-iana" object tag, as per [https://www.rfc-editor.org/rfc/rfc8521.html](https://www.rfc-editor.org/rfc/rfc8521.html)
+_(the -iana object tag as not been registered with IANA)_.
 
 # USAGE
 
         rarrdap.pl DIRECTORY
 
-`DIRECTORY` is the location on disk where the files should be written. `rarrdap.pl` will write
-its working files to this directory as well as the finished .json files.
+`DIRECTORY` is the location on disk where the files should be written.
+`rarrdap.pl` will write the .json files to this directory.
 
 If `DIRECTORY` is not provided, the current directory is used.
 
 # COPYRIGHT
 
-Copyright 2018 CentralNic Ltd. All rights reserved.
+Copyright (c) 2018-2023 CentralNic Ltd and contributors. All rights reserved.
 
 # LICENSE
 
-Copyright (c) 2018 CentralNic Ltd. All rights reserved. This program is
-free software; you can redistribute it and/or modify it under the same
-terms as Perl itself.
+Copyright (c) 2018-2023 CentralNic Ltd and contributors. All rights reserved.
+This program is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
--- rarrdap.pl
+++ rarrdap.pl
@@ -1,19 +1,15 @@
 #!/usr/bin/perl
-# Copyright (c) 2018 CentralNic Ltd. All rights reserved. This program is
-# free software; you can redistribute it and/or modify it under the same
-# terms as Perl itself.
+# Copyright (c) 2018-2023 CentralNic Ltd and contributors. All rights reserved.
+# This program is free software; you can redistribute it and/or modify it under
+# the same terms as Perl itself.
 use Cwd;
+use Data::Mirror qw(mirror_file);
 use DateTime;
-use File::Basename qw(dirname basename);
-use File::stat;
-use IO::File;
 use Getopt::Long;
+use IO::File;
 use JSON;
-use List::MoreUtils qw(uniq);
-use LWP::UserAgent;
-use Text::CSV_XS qw(csv);
-use constant IANA_LIST => 'https://www.iana.org/assignments/registrar-ids/registrar-ids-1.csv';
-use constant INTERNIC_LIST => 'https://www.internic.net/registrars.csv';
+use open qw(:utf8);
+use utf8;
 use strict;
 
 my $VERSION = '0.1';
@@ -30,72 +26,31 @@ if (!-e $dir || !-d $dir) {
 	exit(1);
 }
 
-my $ua = LWP::UserAgent->new('agent' => sprintf('%s/%s', basename(__FILE__, '.pl'), $VERSION));
-
 my $json = JSON->new->pretty->canonical;
-
-my $iana_list = join('/', $dir, basename(IANA_LIST));
-if (!-e $iana_list || stat($iana_list)->mtime <= time()-86400) {
-	print STDERR "Updating registrar list from IANA\n";
-	my $response = $ua->mirror(IANA_LIST, $iana_list);
-
-	if ($response->is_error) {
-		if (-e $iana_list) {
-			warn($response->status_line);
-
-		} else {
-			die($response->status_line);
-
-		}
-
-	} else {
-		utime(undef, undef, $iana_list);
-
-	}
-}
-
-my $iana = {};
-foreach my $row (@{csv('in' => $iana_list, 'headers' => 'auto')}) {
-	$iana->{$row->{'ID'}} = $row;
-}
-
-my $internic_list = join('/', $dir, basename(INTERNIC_LIST));
-if (!-e $internic_list || stat($internic_list)->mtime <= time()-86400) {
-	print STDERR "Updating registrar list from Internic\n";
-	my $response = $ua->mirror(INTERNIC_LIST, $internic_list);
-
-	if ($response->is_error) {
-		if (-e $internic_list) {
-			warn($response->status_line);
-
-		} else {
-			die($response->status_line);
-
-		}
-
-	} else {
-		utime(undef, undef, $internic_list);
-
-	}
-}
-
-my $internic = {};
-foreach my $row (@{csv('in' => $internic_list, 'headers' => 'auto')}) {
-	$internic->{$row->{'iana_id'}} = $row;
-}
 
 my $all = {
   'rdapConformance' => [ 'rdap_level_0' ],
   'entitySearchResults' => [],
 };
 
-foreach my $id (sort({ $a <=> $b } uniq(keys(%{$iana}), keys(%{$internic})))) {
-	next if ('Terminated' eq $iana->{$id}->{'Status'});
+my $file = mirror_file('https://www.icann.org/en/accredited-registrars', 3600 * 6);
 
-	my $object = {
-		'iana' => $iana->{$id},
-		'internic' => $internic->{$id},
-	};
+my $doc = XML::LibXML->load_html(
+    'location'  => $file,
+    'recover'   => 2,
+    'huge'      => 1,
+    'encoding'  => 'UTF-8',
+);
+
+my $data = (grep { 'serverApp-state' eq $_->getAttribute('id') && 'application/json' eq $_->getAttribute('type') } $doc->getElementsByTagName('script'))[0]->childNodes->item(0)->data;
+$data =~ s/\&q;/"/g;
+
+my $object = $json->decode($data);
+
+my $rars = $object->{'accredited-registrars-{"languageTag":"en","siteLanguageTag":"en","slug":"accredited-registrars"}'}->{'data'}->{'accreditedRegistrarsOperations'}->{'registrars'};
+
+foreach my $rar (sort { $a->{'ianaNumber'} <=> $b->{'ianaNumber'} } @{$rars}) {
+    my $id = $rar->{'ianaNumber'};
 
 	my $data = {
 		'objectClassName' => 'entity',
@@ -111,37 +66,31 @@ foreach my $id (sort({ $a <=> $b } uniq(keys(%{$iana}), keys(%{$internic})))) {
 		] ] ],
 	};
 
-	if ($internic->{$id}->{'NAME'}) {
-		push(@{$data->{'vcardArray'}->[1]}, [ 'fn', {}, 'text', $internic->{$id}->{'NAME'} ]);
-		push(@{$data->{'vcardArray'}->[1]}, [ 'org', {}, 'text', $iana->{$id}->{'Registrar Name'} ]);
+	if ($rar->{'publicContact'}->{'name'}) {
+		push(@{$data->{'vcardArray'}->[1]}, [ 'fn', {}, 'text', $rar->{'publicContact'}->{'name'} ]);
+		push(@{$data->{'vcardArray'}->[1]}, [ 'org', {}, 'text', $rar->{'name'} ]);
 
 	} else {
-		push(@{$data->{'vcardArray'}->[1]}, [ 'fn', {}, 'text', $iana->{$id}->{'Registrar Name'} ]);
+		push(@{$data->{'vcardArray'}->[1]}, [ 'fn', {}, 'text', $rar->{'name'} ]);
 
 	}
 
-	if ($internic->{$id}->{'PHONE'}) {
-		$internic->{$id}->{'PHONE'} =~ s/^="//g;
-		$internic->{$id}->{'PHONE'} =~ s/"$//g;
-		push(@{$data->{'vcardArray'}->[1]}, [ 'tel', {} , 'text', $internic->{$id}->{'PHONE'} ]);
+	if ($rar->{'publicContact'}->{'phone'}) {
+		$rar->{'publicContact'}->{'phone'} =~ s/^="//g;
+		$rar->{'publicContact'}->{'phone'} =~ s/"$//g;
+		push(@{$data->{'vcardArray'}->[1]}, [ 'tel', {} , 'text', $rar->{'publicContact'}->{'phone'} ]);
 	};
 
-	push(@{$data->{'vcardArray'}->[1]}, [ 'email', {} , 'text', $internic->{$id}->{'EMAIL'} ]) if ($internic->{$id}->{'EMAIL'});
-	push(@{$data->{'vcardArray'}->[1]}, [ 'adr', {} , 'text', [ '', '', '', '', '', '', $internic->{$id}->{'country_name'} ] ]) if ($internic->{$id}->{'country_name'});
+	push(@{$data->{'vcardArray'}->[1]}, [ 'email', {} , 'text', $rar->{'publicContact'}->{'email'} ]) if ($rar->{'publicContact'}->{'email'});
+	push(@{$data->{'vcardArray'}->[1]}, [ 'adr', {} , 'text', [ '', '', '', '', '', '', $rar->{'country'} ] ]) if ($rar->{'country'});
 
-	if ($internic->{$id}->{'URL'}) {
-		$internic->{$id}->{'URL'} = 'http://'.$internic->{$id}->{'URL'} unless ($internic->{$id}->{'URL'} =~ /^https?:\/\//);
+	if ($rar->{'url'}) {
 		push(@{$data->{'links'}}, {
 			'title' => "Registrar's Website",
 			'rel' => 'related',
-			'href' => $internic->{$id}->{'URL'},
+			'href' => $rar->{'url'}
 		});
 	}
-
-	push(@{$data->{'remarks'}}, {
-		'title' => 'RAA Version',
-		'description' => [ $internic->{$id}->{'RAA'} ]
-	}) if ($internic->{$id}->{'RAA'});
 
 	$data->{'notices'} = [
 		{
@@ -202,6 +151,7 @@ if (!$file->open($jfile, '>:utf8')) {
 } else {
 	$file->print($json->encode($all));
     $file->close;
+
 }
 
 print STDERR "done\n";
@@ -212,15 +162,16 @@ __END__
 
 =head1 NAME
 
-C<rarrdap.pl> - a script to generate a set of RDAP responses for ICANN-accredited registrars.
+C<rarrdap.pl> - a script to generate a set of RDAP responses for ICANN-accredited
+registrars.
 
 =head1 DESCRIPTION
 
-This script scrapes data from the the IANA registrar ID registry and the Internic site, and
-generates RDAP responses for each ICANN-accredited registrar.
+This script scrapes data from the the ICANN website, and generates RDAP
+responses for each ICANN-accredited registrar.
 
-The RDAP responses are written to disk in a directory which can then be exposed through a web
-server.
+The RDAP responses are written to disk in a directory which can then be exposed
+through a web server.
 
 An example of an RDAP service which provides access to this data may be found at
 L<https://registrars.rdap.org>, for example:
@@ -231,25 +182,26 @@ L<https://registrars.rdap.org>, for example:
 
 =back
 
-Entity handles have the "-iana" object tag, as per L<https://www.rfc-editor.org/rfc/rfc8521.html>.
+Entity handles have the "-iana" object tag, as per L<https://www.rfc-editor.org/rfc/rfc8521.html>
+I<(the -iana object tag as not been registered with IANA)>.
 
 =head1 USAGE
 
 	rarrdap.pl DIRECTORY
 
-C<DIRECTORY> is the location on disk where the files should be written. C<rarrdap.pl> will write
-its working files to this directory as well as the finished .json files.
+C<DIRECTORY> is the location on disk where the files should be written.
+C<rarrdap.pl> will write the .json files to this directory.
 
 If C<DIRECTORY> is not provided, the current directory is used.
 
 =head1 COPYRIGHT
 
-Copyright 2018 CentralNic Ltd. All rights reserved.
+Copyright (c) 2018-2023 CentralNic Ltd and contributors. All rights reserved.
 
 =head1 LICENSE
 
-Copyright (c) 2018 CentralNic Ltd. All rights reserved. This program is
-free software; you can redistribute it and/or modify it under the same
-terms as Perl itself.
+Copyright (c) 2018-2023 CentralNic Ltd and contributors. All rights reserved.
+This program is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
 
 =cut
